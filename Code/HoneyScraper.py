14f538a97597337f22571bc584e9336eb805c47c
Lilith Feer <lilith.feer@bluewin.ch>
2023-01-30 11:11:11 +0100

    before meeting

--- Code/HoneyScraper.py
+++ Code/HoneyScraper.py
@@ -2,9 +2,10 @@
 import asyncio
 import time
 from midas import GasDetector
-import pandas as pd
 import os.path
-from csv import writer
+import argparse
+import smtplib
+from email.mime.text import MIMEText
 from datetime import datetime
 from pytz import timezone
 
@@ -13,119 +14,138 @@ from pytz import timezone
 # https://pypi.org/project/midas/
 # https://github.com/numat/midas
 
-async def get():
-    # todo rename the function to be more specific (ex. get_detector_data)
+async def get_new_data():
     # "with" --> we are using a context manager --> prevents a lot of boiler plate code
-    async with GasDetector(IPAdress) as detector:
-        print(await detector.get())
+    async with GasDetector(IPAddress) as detector:
+        #print(await detector.get_new_data())
         # return current state
         return await detector.get()
 
 
 # call this part of code every x minutes
 # flush --> write it to the file (.csv)
-# todo rename function to be more specific (ex. write_detection_dict_to_csv)
-async def write_information(dictionary, path,
-                            values_to_write = ('ip', 'connected', 'state', 'fault', 'alarm', 'concentration',
-                                               'units', 'temperature', 'life', 'flow', 'low-alarm threshold',
-                                               'high-alarm threshold', 'time')):
-    # this is potentially problematic, not sure the values will be always in the same order
-    # We won't know if something has gone wrong, it will just keep writing all the values
-    row = dictionary.values()
-    # todo replace the line above with the following:
-    # row = [dictionary[key] for key in values_to_write]
-    # you can also add try/except clause to catch potential KeyError
+async def write_dict_to_csv(path, values_to_write, error):
+    sui_time = datetime.now(switzerland)
+    if type(error)==list:
+        # we need this because async.gather returns a list of values (one for each function)
+        error=error[1]
 
-    # open the existing csv file in append mode
-    # newline = '' prevents that there are empty lines in between the newly written rows
+    try:
+        # update the dictionary
+        # call function directly, since the event loop was already called
+        dictionary = await get_new_data()
+        # just print the value of fault not a dictionary
+        dictionary["fault"] = dictionary["fault"]["status"]
+        # add current time to dictionary
 
-    # todo simplify the code below
-    # not sure why you are using such complicated code, with context manager you do not need to closes the file for you
-    # also you do not need csv module for this task
-    # can be replaced with this:
+        dictionary["time"] = sui_time.strftime('%Y-%m-%d_%H-%M-%S')
+        # values will be always in the same order
+        # try/except clause to catch potential KeyError
+        try:
+            row = [dictionary[key] for key in values_to_write]
+        except:
+            print("An unexpected key was provided in the dictionary")
 
-    #with open(path, "a") as obj:
-    #   obj.write(",".join(row))
+    # in case of connection loss
+    except:
+        # error counter for exception --> only do e-mail for the first exception
+        error = error + 1
+        #print(error)
+        row=['NA']*len(values_to_write)
+        if error == 1:
+            print("Connection was lost: e-mail is being sent.")
 
-    with open(path, "a", newline='') as obj:
-        # pass this object to writer (converts data into a delimited string) and get a writer object
-        writer_object = writer(obj)
+    # happens anyways
+    finally:
+        # needed to join the values
+        row = [str(r) if type(r) != str else r for r in row]
+        #print(row)
 
-        # write the row
-        writer_object.writerow(row)
+        # write the row with the new values
+        with open(path, "a", newline='') as obj:
+            obj.write(",".join(row) + "\n")
+        print(sui_time.strftime('%Y-%m-%d_%H-%M-%S'))
+    return error
 
-        #close the object
-        obj.close()
 
+# call write_dict_to_csv all x seconds
+async def call_function(values_to_write, path, interval, periodic_function, error):
 
-# call write_information all x seconds
-async def call_function(dictionary, path, interval, periodic_function):
-    # todo this is your main function, it should be repeated calling get() -> otherwise how is dictionary updated?
     while True:
         # prints time since beginning when it is running
-        print(round(time.time() - orig_start_time, 1), "Writing information to csv file...")
+        print("Writing information to csv file...")
 
         # asyncio.gather() allows the caller to group multiple awaitables together
         # once grouped the awaitables can be executed concurrently, awaited and cancelled
         # it also prevents drift in the intervals (drift = when time doesn't match a reference time, very common
         # in computationally intensive processes)
         # very small drifts can still happen
-        await asyncio.gather(
+        error = await asyncio.gather(
             asyncio.sleep(interval),
-            # todo call get() function to get the new dictionary values i.e. lines 94 to 98
-
-            periodic_function(dictionary, path),
+            periodic_function(path, values_to_write, error),
         )
 
 if __name__ == '__main__':
-    # todo look into argparse module -> these arguments should be specified by the user, with values below set as defaults
+    # the values that can be specified by the user
+    parser = argparse.ArgumentParser(prog='HoneyScraper',
+                                     description="Get information from the Honeywell Midas Gas Detector")
+    parser.add_argument("-f", "--filename", default="HoneywellValues.csv",
+                        help="Name of the file where the information is stored. Example: values.csv")
+    parser.add_argument("-d", "--directory", default=os.path.dirname(os.path.realpath(__file__)),
+                        help="Directory in which the file should be saved. Default: location of the script")
+    parser.add_argument("-a", "--ipaddress", type=str, default='169.254.60.47',
+                        help="IP Address of the gas detector. Default: 169.254.60.47")
+    parser.add_argument("-i", "--interval", type=float, default=300,
+                        help="Time distance between the measurements in seconds. Default: 300")
+    parser.add_argument("-v", "--values", nargs='+',
+                        default=('ip', 'connected', 'state', 'fault', 'alarm', 'concentration', 'units', 'temperature',
+                                 'life', 'flow', 'low-alarm threshold', 'high-alarm threshold', 'time'),
+                        help="Columns to write to the file. Options: ip, connected, state, fault, "
+                             "alarm, concentration, units, temperature, life, flow, low-alarm threshold,"
+                             "high-alarm threshold, time")
+    # run the parser and place the extracted data in an argparse.Namespace object
+    args = parser.parse_args()
+
     # variables
-    filename = "HoneyWellValues.csv"
-    directory = "C:/Users/lilit/Git_repos/HoneyScraper/Code/"
+    filename = args.filename
+    directory = args.directory # "C:/Users/lilit/Git_repos/HoneyScraper/Code/"
     # create path from variables
+    print(str("Saving " + filename + " to " + directory))
     path = os.path.join(directory, filename)
-    IPAdress = '169.254.60.47'
-    interval = 10  # seconds
+    IPAddress = args.ipaddress
+    #IPAddress='169.254.60.47'
+    interval = args.interval  # seconds
     switzerland = timezone('Europe/Zurich')
-    sui_time = datetime.now(switzerland)
+    # order of the columns and which columns we want
+    values_to_write = args.values
+    # how many times the error occurred
+    error = 0
 
-    # todo this logic should be inside your main function not here
+    # write the title
+    with open(path, "a", newline='') as obj:
+        obj.write(",".join(values_to_write) + "\n")
+
     # starts the event loop and runs the coroutine (coroutines allow multitasking)
-    valuedictionary = asyncio.run(get())
-    # just print the value of fault not a dictionary
-    valuedictionary["fault"] = valuedictionary["fault"]["status"]
-    # add current time to dictionary
-    valuedictionary["time"] = sui_time.strftime('%Y-%m-%d_%H-%M-%S')
+    try:
+        valuedictionary = asyncio.run(get_new_data())
+        # just print the value of fault not a dictionary
+        valuedictionary["fault"] = valuedictionary["fault"]["status"]
+        # add current time to dictionary
+        sui_time = datetime.now(switzerland)
+        valuedictionary["time"] = sui_time.strftime('%Y-%m-%d_%H-%M-%S')
 
-    # if the file does not exist, save the dictionary as a csv dataframe
+    except:
+        row=['NA']*len(values_to_write)
+        row = [str(r) if type(r) != str else r for r in row]
+        print(row)
+        with open(path, "a", newline='') as obj:
+            obj.write(",".join(row) + "\n")
 
-    # todo won't need this part once this is inside the main function
-    if not os.path.isfile(path):
-        # save it as a pandas dataframe
-        df = pd.DataFrame([valuedictionary])
-        # write
-        df.to_csv(filename, index=False)
-
+    # needed to print the run time in the console
     orig_start_time = time.time()
 
-
-    asyncio.run(call_function(valuedictionary, path, interval, write_information))
-
-#def stop():
-#    task.cancel()
-
-#loop = asyncio.get_event_loop()
-#loop.call_later(interval, stop)
-#task = loop.create_task(write_information(dictionary, path, interval))
-
-# if it is still running, cancel
-#try:
-#    loop.run_until_complete(task)
-#except asyncio.CancelledError:
-#    pass
-
-
-
+    # start the process
+    asyncio.run(call_function(values_to_write, path, interval, write_dict_to_csv, error))
 
 
 
